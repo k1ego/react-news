38a018287d21efb12cd70c249829b52141655088
k1ego <ladonin05@mail.ru>
2024-07-31 17:23:17 +0300

    Migrate from JS to TS

--- src/components/LatestNews/LatestNews.tsx
+++ src/components/LatestNews/LatestNews.tsx
@@ -1,13 +1,17 @@
 import { getLatestNews } from '../../api/apiNews';
 import { useFetch } from '../../helpers/hooks/useFetch';
+import { NewsApiResponse } from '../../Interfaces';
 import BannerListWithSkeleton from '../BannersList/BannersList';
 import styles from './styles.module.css';
 
 const LatestNews = () => {
-  const { data, isLoading } = useFetch(getLatestNews);
+	const { data, isLoading } = useFetch<NewsApiResponse, null>(getLatestNews);
 	return (
 		<section className={styles.section}>
-			<BannerListWithSkeleton banners={data && data.news} isLoading={isLoading} />
+			<BannerListWithSkeleton
+				banners={data && data.news}
+				isLoading={isLoading}
+			/>
 		</section>
 	);
 };
--- src/helpers/hooks/useFetch.ts
+++ src/helpers/hooks/useFetch.ts
@@ -1,9 +1,22 @@
 import { useEffect, useState } from 'react';
 
-export const useFetch = (fetchFunction, params) => {
-	const [data, setData] = useState(null);
-	const [isLoading, setIsLoading] = useState(true);
-	const [error, setError] = useState(null);
+interface FetchFunction<P, T> {
+	(params?: P): Promise<T>;
+}
+
+interface UseFetchResult<T> {
+	data: T | null | undefined;
+	isLoading: boolean;
+	error: Error | null;
+}
+
+export const useFetch = <T, P>(
+	fetchFunction: FetchFunction<P, T>,
+	params?: P
+): UseFetchResult<T> => {
+	const [data, setData] = useState<T | null>(null);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [error, setError] = useState<Error | null>(null);
 
 	// Пример: если params = { name: 'John', age: '30' }, то stringParams станет "name=John&age=30"
 	const stringParams = params ? new URLSearchParams(params).toString() : '';
@@ -15,7 +28,7 @@ export const useFetch = (fetchFunction, params) => {
 				const result = await fetchFunction(params);
 				setData(result);
 			} catch (error) {
-				setError(error);
+				setError(error as Error);
 			} finally {
 				setIsLoading(false);
 			}
