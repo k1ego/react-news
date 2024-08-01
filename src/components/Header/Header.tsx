231f3ff4cf63f5c9cbd50bc97445584db00de410
k1ego <ladonin05@mail.ru>
2024-08-01 20:25:53 +0300

    Implemented a theme change and added useContext

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
       className={`${styles.header} ${isDark ? styles.dark : styles.light}`}
     >
       <div className={styles.info}>
-        <h1 className={styles.title}>NEWS REACTIFY</h1>
+        <h1 className={styles.title}>K1EGO NEWS</h1>
         <p className={styles.date}>{formatDate(new Date())}</p>
       </div>
 
